4c6cd0c03e4e84191f8d04724c91faccc424faa1
Justin Coyne <jcoyne@justincoyne.com>
2015-08-18 12:24:43 -0500

    Generated Document

---
+++ app/actors/curation_concerns/document_actor.rb
@@ -0,0 +1,7 @@
+# Generated via
+#  `rails generate curation_concerns:work Document`
+module CurationConcerns
+  class DocumentActor < CurationConcerns::BaseActor
+    include ::CurationConcerns::WorkActorBehavior
+  end
+end
