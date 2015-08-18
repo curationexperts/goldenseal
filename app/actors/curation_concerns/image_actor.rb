261b21913de323b96ea44b767a1c23dca38dd662
val99erie <val99erie@gmail.com>
2015-08-18 14:03:35 -0500

    Image work type. A-6 from SOW.

---
+++ app/actors/curation_concerns/image_actor.rb
@@ -0,0 +1,7 @@
+# Generated via
+#  `rails generate curation_concerns:work Image`
+module CurationConcerns
+  class ImageActor < CurationConcerns::BaseActor
+    include ::CurationConcerns::WorkActorBehavior
+  end
+end
--- config/initializers/curation_concerns_config.rb
+++ config/initializers/curation_concerns_config.rb
@@ -1,4 +1,6 @@
 CurationConcerns.configure do |config|
+  # Injected via `rails g curation_concerns:work Image`
+  config.register_curation_concern :image
   # Injected via `rails g curation_concerns:work Document`
   config.register_curation_concern :document
 end
