25dc0adcb36536472386189358e3b2aa1f3fd064
Justin Coyne <jcoyne@justincoyne.com>
2015-08-24 14:35:54 -0500

    Fix lookup for original file

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -3,15 +3,12 @@ Riiif::Image.info_service = lambda do |id, file|
   ImageInfo.new(id).dimensions
 end
 
-def logger
-  Rails.logger
-end
-
 Riiif::Image.file_resolver.id_to_uri = lambda do |id|
-  ActiveFedora::Base.id_to_uri(CGI.unescape(id)).tap do |url|
-    logger.info "Riiif resolved #{id} to #{url}"
+  Rails.cache.fetch("#{id}/original_file", expires_in: 3.months) do
+    GenericFile.find(CGI.unescape(id)).original_file.uri
   end
 end
+
 Riiif::Image.file_resolver.basic_auth_credentials = [ActiveFedora.fedora.user, ActiveFedora.fedora.password]
 
 Riiif::Engine.config.cache_duration_in_days = 365
