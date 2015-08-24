0dbce1642a7e7a47596f73b2f1098a94610c2aa9
Justin Coyne <jcoyne@justincoyne.com>
2015-08-24 09:25:27 -0500

    Update config to work with current version of curation_concern

--- config/initializers/curation_concerns.rb
+++ config/initializers/curation_concerns.rb
@@ -1,5 +1,10 @@
-CurationConcerns.config do |config|
+CurationConcerns.configure do |config|
 
+  # Injected via `rails g curation_concerns:work Document`
+  config.register_curation_concern :audio
+  config.register_curation_concern :document
+  config.register_curation_concern :image
+  config.register_curation_concern :video
   config.fits_to_desc_mapping = {
     file_title: :title,
     file_author: :creator
--- config/initializers/curation_concerns_config.rb
+++
@@ -1,7 +0,0 @@
-CurationConcerns.configure do |config|
-  # Injected via `rails g curation_concerns:work Document`
-  config.register_curation_concern :audio
-  config.register_curation_concern :document
-  config.register_curation_concern :image
-  config.register_curation_concern :video
-end
