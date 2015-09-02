390a7f7ad21ac9d4093ab2cc9b1433988b90c25b
val99erie <val99erie@gmail.com>
2015-09-02 10:45:26 -0500

    Cleanup. Stories #96 & #84

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,8 @@
 module ApplicationHelper
 
-  def formatted_system_create(search_results)
-    solr_doc = search_results.fetch(:document)
-    nice_timestamp(solr_doc['system_create_dtsi'])
-  end
-
-  def formatted_system_modified(search_results)
-    solr_doc = search_results.fetch(:document)
-    nice_timestamp(solr_doc['system_modified_dtsi'])
-  end
-
-  def nice_timestamp(datetime)
-    datetime.in_time_zone.strftime("%F %T %Z")
+  def formatted_time(search_results)
+    datetime = search_results.fetch(:value)
+    datetime.in_time_zone.strftime("%F %R %Z")
   end
 
 end
