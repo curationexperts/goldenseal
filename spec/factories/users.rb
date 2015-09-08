632857cb7eebedb00bb0dcbc262b0d9df444f715
Justin Coyne <jcoyne@justincoyne.com>
2015-09-08 12:16:05 -0500

    Don't lookup groups in tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,12 @@ FactoryGirl.define do
     sequence :username do |n|
       "person_#{n}"
     end
+
+    group_list []
+
+    # Prevent ldap from being called
+    groups_list_expires_at { 1.day.from_now }
+
+
   end
 end
