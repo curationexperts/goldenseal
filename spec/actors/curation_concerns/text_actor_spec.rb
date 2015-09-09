ffc50ab2051cb5df04ab444b847127d1a5cd251e
val99erie <val99erie@gmail.com>
2015-09-09 14:56:43 -0500

    Removed auto-generated pending specs

--- spec/actors/curation_concerns/text_actor_spec.rb
+++ spec/actors/curation_concerns/text_actor_spec.rb
@@ -1,9 +1,4 @@
-# Generated via
-#  `rails generate curation_concerns:work Text`
 require 'rails_helper'
 
 describe CurationConcerns::TextActor do
-  it "should have some tests" do
-    skip "Add your tests here"
-  end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,9 +1,4 @@
-# Generated via
-#  `rails generate curation_concerns:work Image`
 require 'rails_helper'
 
 describe Image do
-  it "should have some tests" do
-    skip "Add your tests here"
-  end
 end
