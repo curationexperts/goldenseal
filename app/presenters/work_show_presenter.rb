bb9489c007b306e1cddc0f3731db0c874349a35c
val99erie <val99erie@gmail.com>
2015-09-11 10:37:40 -0500

    Fixed nil case

--- app/presenters/work_show_presenter.rb
+++ app/presenters/work_show_presenter.rb
@@ -2,6 +2,6 @@ class WorkShowPresenter < CurationConcerns::GenericWorkShowPresenter
   include DisplayFields
 
   def tei_id
-    solr_document['hasEncodedText_ssim'].first
+    Array(solr_document['hasEncodedText_ssim']).first
   end
 end
