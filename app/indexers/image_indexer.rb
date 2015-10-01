06a14bc93651b673f735f597a4d645761c4c42a9
Justin Coyne <jcoyne@justincoyne.com>
2015-10-01 12:04:51 -0500

    Capture blacklight exceptions that result from reindex_everything

--- app/indexers/image_indexer.rb
+++ app/indexers/image_indexer.rb
@@ -1,7 +1,11 @@
 class ImageIndexer < BaseWorkIndexer
   def generate_solr_document
     super do |solr_doc|
-      solr_doc.merge! ImageInfo.new(object.representative).solr_document if object.representative
+      begin
+        solr_doc.merge! ImageInfo.new(object.representative).solr_document if object.representative
+      rescue Blacklight::Exceptions::RecordNotFound
+        Rails.logger.warn "Unable to index the representative image (#{object.representative}) for #{object.id}. If you are running 'reindex_everything', run it again once it finishes.  Otherwise RIIIF will be unable to find this image"
+      end
     end
   end
 end
