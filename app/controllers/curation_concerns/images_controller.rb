55170ca73b5b9280760127a65252622e8097f614
Justin Coyne <jcoyne@justincoyne.com>
2015-10-09 07:54:46 -0500

    Search for the presenter in root namespace

--- app/controllers/curation_concerns/images_controller.rb
+++ app/controllers/curation_concerns/images_controller.rb
@@ -1,11 +1,8 @@
-# Generated via
-#  `rails generate curation_concerns:work Image`
-
 class CurationConcerns::ImagesController < ApplicationController
   include CurationConcerns::CurationConcernController
   set_curation_concern_type Image
 
   def show_presenter
-    WorkShowPresenter
+    ::WorkShowPresenter
   end
 end
