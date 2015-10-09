9ee90f8577f71c7c66464c4a7c8c588737383e8d
Justin Coyne <jcoyne@justincoyne.com>
2015-10-08 20:11:49 -0500

    Upgrade to latest curation_concerns

--- Gemfile
+++ Gemfile
@@ -27,8 +27,7 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-# For https://github.com/projecthydra-labs/curation_concerns/pull/377
-gem 'curation_concerns', github: 'projecthydra-labs/curation_concerns', ref: 'da72aa9'
+gem 'curation_concerns', github: 'projecthydra-labs/curation_concerns', ref: 'eff40fc'
 
 # We need to pin resque-web to the master branch on github
 # because the most recent release is long out-of-date.
--- app/controllers/admin_sets_controller.rb
+++ app/controllers/admin_sets_controller.rb
@@ -22,26 +22,4 @@ class AdminSetsController < ApplicationController
         :title, :identifier, :description, creator: [], contributor: [],
         subject: [], publisher: [], language: [])
     end
-
-    # This can be removed when https://github.com/projecthydra-labs/curation_concerns/pull/376 is merged
-    def deny_access(exception)
-      if current_user && current_user.persisted?
-        respond_to do |wants|
-          wants.html do
-            if [:show, :edit, :update, :destroy].include? exception.action
-              render 'curation_concerns/base/unauthorized', status: :unauthorized
-            else
-              redirect_to root_url, alert: exception.message
-            end
-          end
-          wants.json { render_json_response(response_type: :forbidden, message: exception.message) }
-        end
-      else
-        session['user_return_to'.freeze] = request.url
-        respond_to do |wants|
-          wants.html { redirect_to new_user_session_path, alert: exception.message }
-          wants.json { render_json_response(response_type: :unauthorized, message: exception.message) }
-        end
-      end
-    end
 end
