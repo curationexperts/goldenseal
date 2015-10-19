fdca85b4e8baf039d8a0752fb1ebae1604269ed6
Justin Coyne <jcoyne@justincoyne.com>
2015-10-19 11:29:36 -0500

    Unauthenticated users should see "View Members" link on admin sets. Fixes #205

--- spec/views/admin_sets/show.html.erb_spec.rb
+++ spec/views/admin_sets/show.html.erb_spec.rb
@@ -13,13 +13,32 @@ describe 'admin_sets/show.html.erb' do
   before do
     assign(:presenter, AdminSetPresenter.new(solr_doc))
     allow(view).to receive(:blacklight_config).and_return(config)
-    allow(view).to receive(:can?).and_return(true)
     expect(view).to receive(:render_thumbnail_tag).with(solr_doc)
-    render
   end
 
-  it "displays metadata" do
-    expect(rendered).to have_selector 'span.label.label-danger', text: 'Private'
-    expect(rendered).to have_link 'View Members', href: '/catalog?f%5Badmin_set_ssi%5D%5B%5D=All+the+things'
+  context "as an admin" do
+    before do
+      allow(view).to receive(:can?).and_return(true)
+      render
+    end
+    it "displays metadata" do
+      expect(rendered).to have_selector 'span.label.label-danger', text: 'Private'
+      expect(rendered).to have_link 'View Members', href: '/catalog?f%5Badmin_set_ssi%5D%5B%5D=All+the+things'
+      expect(rendered).to have_link 'Delete Collection'
+      expect(rendered).to have_link 'Edit Metadata'
+    end
+  end
+
+  context "as a regular user" do
+    before do
+      allow(view).to receive(:can?).and_return(false)
+      render
+    end
+    it "displays metadata" do
+      expect(rendered).to have_selector 'span.label.label-danger', text: 'Private'
+      expect(rendered).to have_link 'View Members', href: '/catalog?f%5Badmin_set_ssi%5D%5B%5D=All+the+things'
+      expect(rendered).not_to have_link 'Delete Collection'
+      expect(rendered).not_to have_link 'Edit Metadata'
+    end
   end
 end
