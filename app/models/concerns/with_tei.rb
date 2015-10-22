f0c2194141835ca4c92169f7bb0fb29c372d27de
Justin Coyne <jcoyne@justincoyne.com>
2015-10-21 22:47:56 -0500

    Update to curation_concerns 0.2.0

--- app/models/concerns/with_tei.rb
+++ app/models/concerns/with_tei.rb
@@ -9,8 +9,9 @@ module WithTEI
 
   # Use the first file if tei isn't set and the first file is an xml
   def assign_default_tei
-    return if tei_id || !head
-    first_file = head.target
+    return if tei_id || members.empty?
+    # TODO: https://github.com/projecthydra-labs/activefedora-aggregation/issues/95
+    first_file = ordered_members.to_a.first
     self.tei_id = first_file.id if ['application/xml', 'text/xml'].include?(first_file.mime_type)
   end
 end
