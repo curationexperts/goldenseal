c506c4557be8a38f95febda91d7949f8efc0de9c
Justin Coyne <jcoyne@justincoyne.com>
2015-10-27 11:15:14 -0500

    Fix controller spec

--- spec/controllers/curation_concerns/images_controller_spec.rb
+++ spec/controllers/curation_concerns/images_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe CurationConcerns::ImagesController do
-  let(:user) { create(:user) }
+  let(:user) { create(:admin) }
   let(:admin_set) { create(:admin_set) }
   before { sign_in user }
   describe "#create" do
     it "creates Images" do
       expect {
         post :create, "image" => { "title" => ["A portrait"],
-                                   "admin_set_id"=>admin_set.id }
+                                   "admin_set_id" => admin_set.id }
       }.to change { Image.count }.by(1)
 
       expect(assigns[:curation_concern].admin_set).to eq admin_set
