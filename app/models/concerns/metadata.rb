d12e7727e04a6d1c704ecc1c38ef0ef189842b53
Justin Coyne <jcoyne@justincoyne.com>
2015-11-02 20:52:29 -0600

    Don't hint that this is a date. We're storing strings in date_issued

--- app/models/concerns/metadata.rb
+++ app/models/concerns/metadata.rb
@@ -3,7 +3,6 @@ module Metadata
 
   included do
     property :date_issued, predicate: ::RDF::DC.issued, multiple: false do |index|
-      index.type :date
       index.as :stored_sortable
     end
 
