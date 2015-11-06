afac0fcbe375d50c51537c8966303dc4f926ff1c
val99erie <val99erie@gmail.com>
2015-11-06 14:20:14 -0600

    Select files for texts. Story #226

--- app/controllers/curation_concerns/texts_controller.rb
+++ app/controllers/curation_concerns/texts_controller.rb
@@ -3,6 +3,10 @@
 
 class CurationConcerns::TextsController < ApplicationController
   include CurationConcerns::CurationConcernController
+
+  include AttachFiles
+  before_filter :add_attachments_to_files, only: :create
+
   set_curation_concern_type Text
 
   # Gives the class of the show presenter. Override this if you want
