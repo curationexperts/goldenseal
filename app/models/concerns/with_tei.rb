cb2228d22618db38ced592611fdc7625a85bb3e4
Justin Coyne <jcoyne@justincoyne.com>
2015-12-21 10:19:30 -0600

    Don't set a TEI if none exist in the ordered members. Fixes #239

--- app/models/concerns/with_tei.rb
+++ app/models/concerns/with_tei.rb
@@ -9,9 +9,13 @@ module WithTEI
 
   # Use the first file if tei isn't set and the first file is an xml
   def assign_default_tei
-    return if tei_id || members.empty?
-    # TODO: https://github.com/projecthydra-labs/activefedora-aggregation/issues/95
+    return if tei_id
+
+    # TODO: the Order::TargetProxy should respond to #first
+    # https://github.com/projecthydra-labs/activefedora-aggregation/issues/95
     first_file = ordered_members.to_a.first
+    return unless first_file
+
     self.tei_id = first_file.id if ['application/xml', 'text/xml'].include?(first_file.mime_type)
   end
 end
