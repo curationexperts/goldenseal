7f2661caad00190dd5e645c84d919828dde74f49
MATTHEW CLARK <matt@notch8.com>
2018-10-01 13:42:57 -0700

    dont offer custom meta data fields if no spotligt exhibit on work edit

--- app/controllers/concerns/custom_metadata.rb
+++ app/controllers/concerns/custom_metadata.rb
@@ -5,9 +5,11 @@ module CustomMetadata
     super
     if @form.model.admin_set
       existing_custom_metadatas = @form.model.custom_metadatas.collect{|d| d.title.titleize}
-      @form.model.admin_set.spotlight_exhibit.custom_fields.each do |custom_field|
-        unless existing_custom_metadatas.include? custom_field.configuration["label"]
-          @form.model.custom_metadatas.build(title: custom_field.slug.humanize)
+      unless @form.model.admin_set.spotlight_exhibit.nil?
+        @form.model.admin_set.spotlight_exhibit.custom_fields.each do |custom_field|
+          unless existing_custom_metadatas.include? custom_field.configuration["label"]
+            @form.model.custom_metadatas.build(title: custom_field.slug.humanize)
+          end
         end
       end
     end
