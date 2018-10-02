3845e36a26a494b7dcc4495daf7e2f42a6bb193f
MATTHEW CLARK <matt@notch8.com>
2018-10-02 15:23:02 -0700

    if instead of unless

--- app/controllers/concerns/custom_metadata.rb
+++ app/controllers/concerns/custom_metadata.rb
@@ -5,7 +5,7 @@ module CustomMetadata
     super
     if @form.model.admin_set
       existing_custom_metadatas = @form.model.custom_metadatas.collect{|d| d.title.titleize}
-      unless @form.model.admin_set.spotlight_exhibit.nil?
+      if @form.model.admin_set.spotlight_exhibit.present?
         @form.model.admin_set.spotlight_exhibit.custom_fields.each do |custom_field|
           unless existing_custom_metadatas.include? custom_field.configuration["label"]
             @form.model.custom_metadatas.build(title: custom_field.slug.humanize)
